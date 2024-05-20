95c3b4b8b717f4a00539b4712b0c2a2054c6f2aa
ValeriiaPiskunova <valeriia.piskunova@gmail.com>
2024-05-20 12:36:58 +0200

    hw fixed

--- hw21_objects/script.js
+++ hw21_objects/script.js
@@ -1,17 +1,24 @@
 // мінімум
 
-let car = {
-  brand: "Fiat",
-  model: "Abarth 595",
-  manufactureYear: "2020",
-  speedAvg: "150",
-  fuelVolume: "35",
-  fuelConsumptionAvg: "7.1",
-  drivers: ["Alex"],
+function Car(
+  brand,
+  model,
+  manufactureYear,
+  speedAvg,
+  fuelVolume,
+  fuelConsumptionAvg,
+  drivers
+) {
+  this.brand = brand;
+  this.model = model;
+  this.manufactureYear = manufactureYear;
+  this.speedAvg = speedAvg;
+  this.fuelVolume = fuelVolume;
+  this.fuelConsumptionAvg = fuelConsumptionAvg;
+  this.drivers = drivers;
 
   //    Виводить на екран інформацію про автомобіль.
-
-  displayInfo() {
+  this.displayInfo = () => {
     console.log(`Виробник: ${this.brand}`);
     console.log(`Модель: ${this.model}`);
     console.log(`Рік випуску: ${this.manufactureYear}`);
@@ -21,36 +28,33 @@ let car = {
       `Середня витрата палива: ${this.fuelConsumptionAvg} л на 100 км`
     );
     console.log(`Водії: ${this.drivers}`);
-  },
+  };
 
   // Додавання ім’я водія у список
-
-  addDrivers(newDriver) {
+  this.addDrivers = (newDriver) => {
     if (!this.drivers.includes(newDriver)) {
       this.drivers.push(newDriver);
       console.log(`${newDriver} доданий до списку водіїв.`);
     } else {
       console.log(`${newDriver} вже є у списку.`);
     }
-  },
+  };
 
   // Перевірка водія на наявність у списку
-
-  checkDrivers(newDriver) {
+  this.checkDrivers = (newDriver) => {
     if (this.drivers.includes(newDriver)) {
       console.log(`${newDriver} вже є у списку водіїв.`);
     } else {
       console.log(`${newDriver} немає у списку водіїв.`);
     }
-  },
+  };
 
   // Підрахунок необхідного часу та кількості палива
-
-  tripCalculation(distance) {
-    timeWithoutRest = Number(distance / car.speedAvg);
+  this.tripCalculation = (distance) => {
+    timeWithoutRest = Number(distance / this.speedAvg);
     countOfRestings = Math.floor(timeWithoutRest / 4);
     generalTimeOfTrip = timeWithoutRest + countOfRestings;
-    fuelNeeded = Number((distance / 100) * car.fuelConsumptionAvg);
+    fuelNeeded = Number((distance / 100) * this.fuelConsumptionAvg);
     console.log(
       `Для подолання відстані ${distance} (в км) необхідно ${fuelNeeded.toFixed(
         1
@@ -58,52 +62,69 @@ let car = {
         1
       )} год з них відпочинок ${countOfRestings} год.`
     );
-  },
-};
-
-car.displayInfo();
-
-car.addDrivers("Andrii");
-car.addDrivers("Mariia");
-car.addDrivers("Mariia");
-car.addDrivers("Alex");
-
-car.checkDrivers("Alex");
-car.checkDrivers("Max");
-car.checkDrivers("Mariia");
-
-car.tripCalculation(200);
-car.tripCalculation(700);
-car.tripCalculation(750);
-car.tripCalculation(1500);
-
-//норма
-
-const today = new Date();
-
-function displayTime(day) {
-  hours = day.getHours();
-  minutes = day.getMinutes();
-  seconds = day.getSeconds();
-  console.log(
-    `Поточний час ${hours} годин, ${minutes} хвилин і ${seconds} секунд. `
-  );
+  };
 }
 
-function changeHours(hoursToAdd) {
-  today.setHours(today.getHours() + hoursToAdd);
+let abarth = new Car(
+  "Fiat",
+  "Abarth 595",
+  "2020",
+  "150",
+  "35",
+  "7.1",
+  ["Alex"]
+);
+
+abarth.displayInfo();
+
+abarth.addDrivers("Andrii");
+abarth.addDrivers("Mariia");
+abarth.addDrivers("Mariia");
+abarth.addDrivers("Alex");
+
+abarth.checkDrivers("Alex");
+abarth.checkDrivers("Max");
+abarth.checkDrivers("Mariia");
+
+abarth.tripCalculation(200);
+abarth.tripCalculation(700);
+abarth.tripCalculation(750);
+abarth.tripCalculation(1500);
+
+// норма
+
+function DateHelper(date) {
+  this.date = date;
+
+  this.displayTime = () => {
+    hours = this.date.getHours();
+    minutes = this.date.getMinutes();
+    seconds = this.date.getSeconds();
+    console.log(
+      `Поточний час ${hours} годин, ${minutes} хвилин і ${seconds} секунд.`
+    );
+  };
+
+  this.changeHours = (hoursToAdd) => {
+    this.date.setHours(this.date.getHours() + hoursToAdd);
+  };
+
+  this.changeMinutes = (minutesToAdd) => {
+    this.date.setMinutes(this.date.getMinutes() + minutesToAdd);
+  };
+
+  this.changeSeconds = (secondsToAdd) => {
+    this.date.setSeconds(this.date.getSeconds() + secondsToAdd);
+  };
 }
 
-function changeMinutes(minutesToAdd) {
-  today.setMinutes(today.getMinutes() + minutesToAdd);
-}
+const dateTimeNow = new Date();
+let dateHelper = new DateHelper(dateTimeNow);
 
-function changeSeconds(secondsToAdd) {
-  today.setSeconds(today.getSeconds() + secondsToAdd);
-}
+dateHelper.displayTime();
 
-displayTime(today);
-changeHours(2);
-changeMinutes(30);
-changeSeconds(75);
-console.log(today);
+dateHelper.changeHours(2);
+dateHelper.changeMinutes(30);
+dateHelper.changeSeconds(75);
+
+console.log(dateTimeNow);
